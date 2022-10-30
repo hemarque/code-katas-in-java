ec699fae1ef94314b482a55bf926d2890a8dd6ec
Helder De Oliveira <hemarque@gmail.com>
2022-10-30 15:47:28 +0100

    Refactor: return Fizz is the input is divisible by 3

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,10 +1,14 @@
 public class FizzBuzz {
     public static String of(int number) {
-        if (number == 3)
+        if (isDivisibleBy3(number))
             return "Fizz";
         else if (number == 5) {
             return "Buzz";
         } else
             return number+"";
+    }
+
+    private static boolean isDivisibleBy3(int number) {
+        return number % 3 == 0;
     }
 }
--- FizzBuzz/src/test/java/FizzBuzzShould.java
+++ FizzBuzz/src/test/java/FizzBuzzShould.java
@@ -8,11 +8,15 @@ public class FizzBuzzShould {
         assertEquals("1", FizzBuzz.of(1));
         assertEquals("2", FizzBuzz.of(2));
         assertEquals("4", FizzBuzz.of(4));
+        assertEquals("7", FizzBuzz.of(7));
+        assertEquals("8", FizzBuzz.of(8));
     }
 
     @Test
-    public void returnFizzIfInputIs3() {
+    public void returnFizzIfInputIsDivisibleBy3() {
         assertEquals("Fizz", FizzBuzz.of(3));
+        assertEquals("Fizz", FizzBuzz.of(6));
+        assertEquals("Fizz", FizzBuzz.of(9));
     }
 
 
