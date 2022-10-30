9355550e17331266c0317ddd6d6dd88915bd7f97
Helder De Oliveira <hemarque@gmail.com>
2022-10-30 15:39:40 +0100

    Refactor: FizzBuzz should return the same number if the input is not 3

--- FizzBuzz/src/test/java/FizzBuzzShould.java
+++ FizzBuzz/src/test/java/FizzBuzzShould.java
@@ -4,13 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzShould {
     @Test
-    public void return1IfInputIs1() {
+    public void returnTheSameNumberIfInputIsNot3() {
         assertEquals("1", FizzBuzz.of(1));
-    }
-
-    @Test
-    public void return2IfInputIs2() {
         assertEquals("2", FizzBuzz.of(2));
+        assertEquals("4", FizzBuzz.of(4));
     }
 
     @Test
@@ -18,8 +15,4 @@ public class FizzBuzzShould {
         assertEquals("Fizz", FizzBuzz.of(3));
     }
 
-    @Test
-    public void return4IfInputIs4() {
-        assertEquals("4", FizzBuzz.of(4));
-    }
 }
