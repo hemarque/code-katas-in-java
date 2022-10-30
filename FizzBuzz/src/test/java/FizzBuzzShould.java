8e60df8b0bfb190ab2b0797d0a445c3c87926461
Helder De Oliveira <hemarque@gmail.com>
2022-10-30 15:26:39 +0100

    Refactor: make the Tests more verbose, "FizzBuzz should return 1 if input is 1"

--- FizzBuzz/src/test/java/FizzBuzzShould.java
+++ FizzBuzz/src/test/java/FizzBuzzShould.java
@@ -2,9 +2,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class FizzBuzzTest {
+public class FizzBuzzShould {
     @Test
-    public void fizzbuzzOf1is1() {
+    public void return1IfInputIs1() {
         assertEquals(1, FizzBuzz.of(1));
     }
 
