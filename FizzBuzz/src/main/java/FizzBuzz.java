a966466e9f06b5f09f29ed8cd8dd44f30c31bfc9
Helder De Oliveira <hemarque@gmail.com>
2022-10-31 08:34:38 +0100

    Refactor: from static to instance

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,5 +1,5 @@
 public class FizzBuzz {
-    public static String of(int number) {
+    public String of(int number) {
         if(isDivisibleBy3(number) && isDivisibleBy5(number))
             return "FizzBuzz";
         else if (isDivisibleBy3(number))
@@ -10,11 +10,11 @@ public class FizzBuzz {
             return number+"";
     }
 
-    private static boolean isDivisibleBy5(int number) {
+    private boolean isDivisibleBy5(int number) {
         return number % 5 == 0;
     }
 
-    private static boolean isDivisibleBy3(int number) {
+    private boolean isDivisibleBy3(int number) {
         return number % 3 == 0;
     }
 }
--- FizzBuzz/src/test/java/FizzBuzzShould.java
+++ FizzBuzz/src/test/java/FizzBuzzShould.java
@@ -3,34 +3,35 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzShould {
+    FizzBuzz fizzbuzz = new FizzBuzz();
     @Test
     public void returnTheSameNumberIfInputIsNot3() {
-        assertEquals("1", FizzBuzz.of(1));
-        assertEquals("2", FizzBuzz.of(2));
-        assertEquals("4", FizzBuzz.of(4));
-        assertEquals("7", FizzBuzz.of(7));
-        assertEquals("8", FizzBuzz.of(8));
+        assertEquals("1", fizzbuzz.of(1));
+        assertEquals("2", fizzbuzz.of(2));
+        assertEquals("4", fizzbuzz.of(4));
+        assertEquals("7", fizzbuzz.of(7));
+        assertEquals("8", fizzbuzz.of(8));
     }
 
     @Test
     public void returnFizzIfInputIsDivisibleBy3() {
-        assertEquals("Fizz", FizzBuzz.of(3));
-        assertEquals("Fizz", FizzBuzz.of(6));
-        assertEquals("Fizz", FizzBuzz.of(9));
+        assertEquals("Fizz", fizzbuzz.of(3));
+        assertEquals("Fizz", fizzbuzz.of(6));
+        assertEquals("Fizz", fizzbuzz.of(9));
     }
 
 
     @Test
     public void returnBuzzIfInputIsDivisibleBy5() {
-        assertEquals("Buzz", FizzBuzz.of(5));
-        assertEquals("Buzz", FizzBuzz.of(10));
+        assertEquals("Buzz", fizzbuzz.of(5));
+        assertEquals("Buzz", fizzbuzz.of(10));
     }
 
     @Test
     public void returnFizzBuzzIfInputIsDivisibleBy3and5() {
-        assertEquals("FizzBuzz", FizzBuzz.of(15));
-        assertEquals("FizzBuzz", FizzBuzz.of(30));
-        assertEquals("FizzBuzz", FizzBuzz.of(45));
+        assertEquals("FizzBuzz", fizzbuzz.of(15));
+        assertEquals("FizzBuzz", fizzbuzz.of(30));
+        assertEquals("FizzBuzz", fizzbuzz.of(45));
     }
 
 }
