feab2e84ff9959b86b58c57fabd856b29a427210
Helder De Oliveira <hemarque@gmail.com>
2022-11-01 08:40:05 +0100

    testing the setup, everything seems ok!

---
+++ roman-numerals/src/test/java/RomanNumeralTranslatorShould.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class RomanNumeralTranslatorShould {
+    @Test
+    public void setupTest(){
+        assertTrue(true);
+    }
+}
