b6efd9e0a005a58d41508bcc87946a0535a33981
Helder De Oliveira <hemarque@gmail.com>
2022-11-01 12:14:19 +0100

    Refactor: simplify with a for loop and listing special cases

--- roman-numerals/src/main/java/RomanNumeralTranslator.java
+++ roman-numerals/src/main/java/RomanNumeralTranslator.java
@@ -1,33 +1,15 @@
 public class RomanNumeralTranslator {
+    String[] romans = {"X", "IX", "V", "IV", "I"};
+    int[] arabics = {10, 9, 5, 4, 1};
 
     public String toRoman(int number) {
         String roman = "";
 
-
-        while (number >= 10) {
-            roman = roman + "X";
-            number -= 10;
-        }
-
-        while (number >= 9) {
-            roman = roman + "IX";
-            number -= 9;
-        }
-
-        while (number >= 5) {
-            roman = roman + "V";
-            number -= 5;
-        }
-
-        while (number >= 4) {
-            roman = roman + "IV";
-            number -= 4;
-        }
-
-
-        while (number >= 1) {
-            roman = roman + "I";
-            number -= 1;
+        for(int i = 0; i<arabics.length;i++){
+            while (number >= arabics[i]) {
+                roman = roman + romans[i];
+                number -= arabics[i];
+            }
         }
 
         return roman;
