514f769f4fc2826d5f116c959cb3c5eacb5669d0
Helder De Oliveira <hemarque@gmail.com>
2022-11-01 11:24:18 +0100

    Refactor: looping on I

--- roman-numerals/src/main/java/RomanNumeralTranslator.java
+++ roman-numerals/src/main/java/RomanNumeralTranslator.java
@@ -1,14 +1,11 @@
 public class RomanNumeralTranslator {
 
     public String toRoman(int number) {
-        String response = "";
-        if (number == 3)
-            response = "III";
-        else if (number == 2)
-            response = "II";
-        else
-            response = "I";
+        String roman = "";
 
-        return response;
+        for(int i = 0; i<number;i++)
+            roman = roman + "I";
+
+        return roman;
     }
 }
