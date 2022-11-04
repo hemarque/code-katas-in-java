885f62b98376f81a483de7bad9bbc1ebe3b343af
Helder De Oliveira <hemarque@gmail.com>
2022-11-04 19:05:44 +0100

    refactor: reordering code

--- SimpleGameOfLife/src/main/java/Cell.java
+++ SimpleGameOfLife/src/main/java/Cell.java
@@ -2,8 +2,21 @@ public class Cell {
     private boolean lives = false;
     private int neighbours = 0;
 
+    public Cell nextGeneration() {
+        Cell nextGeneration = new Cell();
+        if (isAlive() && (neighbours == 2 || neighbours == 3))
+            nextGeneration.live();
+        else if (!isAlive() && neighbours == 3)
+            nextGeneration.live();
+        return nextGeneration;
+    }
+
     public void live() {
         this.lives = true;
+    }
+
+    public void die() {
+        this.lives = false;
     }
 
     public boolean isAlive() {
@@ -12,18 +25,5 @@ public class Cell {
 
     public void hasThisNumberOfNeighbours(int neighbours) {
         this.neighbours = neighbours;
-    }
-
-    public Cell nextGeneration() {
-        Cell nextGeneration = new Cell();
-        if (isAlive() && (neighbours == 2 || neighbours == 3))
-            nextGeneration.live();
-        else if(!isAlive() && neighbours==3)
-            nextGeneration.live();
-        return nextGeneration;
-    }
-
-    public void die() {
-        this.lives = false;
     }
 }
