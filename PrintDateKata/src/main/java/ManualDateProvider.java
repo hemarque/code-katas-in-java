f699705a4d06c5902fe76e1488f1905e0efaaff0
Helder De Oliveira <hemarque@gmail.com>
2022-11-06 10:22:25 +0100

    Test the code with doubles created by you: setting the manual date provider

---
+++ PrintDateKata/src/main/java/ManualDateProvider.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+
+public class ManualDateProvider {
+
+    private Date date;
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+}
--- PrintDateKata/src/test/java/DatePrinterShould.java
+++ PrintDateKata/src/test/java/DatePrinterShould.java
@@ -1,10 +1,18 @@
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DatePrinterShould {
     @Test
-    public void setupTest() {
-        assertTrue(true);
+    public void settingDateOnAManualDateProvider() {
+        Date currentDate = Date.valueOf("2022-11-06");
+        ManualDateProvider dateProvider = new ManualDateProvider();
+
+        dateProvider.setDate(currentDate);
+
+        assertEquals(currentDate, dateProvider.getDate());
     }
 }
