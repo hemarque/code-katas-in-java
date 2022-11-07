fbb564afe2f12f3972098b42fbf4b166531f1e21
Helder De Oliveira <hemarque@gmail.com>
2022-11-07 11:26:23 +0100

    refactor: extract method for the new logic "has at least 1 uppercase letter"

--- PasswordValidator/src/main/java/PasswordValidator.java
+++ PasswordValidator/src/main/java/PasswordValidator.java
@@ -1,5 +1,10 @@
 public class PasswordValidator {
     public static boolean validate(String validPassword) {
+        return  hasMoreThan8Characters(validPassword) &&
+                hasAtLeast1Uppercase(validPassword);
+    }
+
+    private static boolean hasAtLeast1Uppercase(String validPassword) {
         boolean hasAtLeast1Uppercase = false;
         for(char c : validPassword.toCharArray()){
             if (c >= 'A' && c <= 'Z'){
@@ -7,7 +12,7 @@ public class PasswordValidator {
                 break;
             }
         }
-        return hasMoreThan8Characters(validPassword) && hasAtLeast1Uppercase;
+        return hasAtLeast1Uppercase;
     }
 
     private static boolean hasMoreThan8Characters(String validPassword) {
