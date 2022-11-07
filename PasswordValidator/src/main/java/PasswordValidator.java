43fde0d1306cc4e1a78f41df4dd81356eb450bd3
Helder De Oliveira <hemarque@gmail.com>
2022-11-07 11:17:49 +0100

    validator returns invalid if password has no uppercase letters

--- PasswordValidator/src/main/java/PasswordValidator.java
+++ PasswordValidator/src/main/java/PasswordValidator.java
@@ -1,6 +1,13 @@
 public class PasswordValidator {
     public static boolean validate(String validPassword) {
-        return hasMoreThan8Characters(validPassword);
+        boolean hasAtLeast1Uppercase = false;
+        for(char c : validPassword.toCharArray()){
+            if (c >= 'A' && c <= 'Z'){
+                hasAtLeast1Uppercase = true;
+                break;
+            }
+        }
+        return hasMoreThan8Characters(validPassword) && hasAtLeast1Uppercase;
     }
 
     private static boolean hasMoreThan8Characters(String validPassword) {
--- PasswordValidator/src/test/java/PasswordValidatorShould.java
+++ PasswordValidator/src/test/java/PasswordValidatorShould.java
@@ -5,15 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PasswordValidatorShould {
     @Test
-    public void returnValidIfPasswordHasMoreThan8Characters(){
-        String validPassword = "123456789";
+    public void returnValidIfPasswordHasMoreThan8Characters() {
+        String validPassword = "A23456789";
         boolean isValid = PasswordValidator.validate(validPassword);
         assertTrue(isValid);
     }
 
     @Test
-    public void returnInvalidIfPasswordHas8CharactersOrLess(){
+    public void returnInvalidIfPasswordHas8CharactersOrLess() {
         String invalidPassword = "12345678";
+        boolean isValid = PasswordValidator.validate(invalidPassword);
+        assertFalse(isValid);
+    }
+
+    @Test
+    public void returnInvalidIfPasswordHasNoUppercaseLetters() {
+        String invalidPassword = "123456789";
         boolean isValid = PasswordValidator.validate(invalidPassword);
         assertFalse(isValid);
     }
