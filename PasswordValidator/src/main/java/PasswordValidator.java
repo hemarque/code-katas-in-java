2ba1173b12e56854d3e777d0926ff208823e6393
Helder De Oliveira <hemarque@gmail.com>
2022-11-07 10:59:41 +0100

    refactor: extract method "has more than 8 characters"

--- PasswordValidator/src/main/java/PasswordValidator.java
+++ PasswordValidator/src/main/java/PasswordValidator.java
@@ -1,5 +1,9 @@
 public class PasswordValidator {
     public static boolean validate(String validPassword) {
+        return hasMoreThan8Characters(validPassword);
+    }
+
+    private static boolean hasMoreThan8Characters(String validPassword) {
         return validPassword.length() > 8;
     }
 }
