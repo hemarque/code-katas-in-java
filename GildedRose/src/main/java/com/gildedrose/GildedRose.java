1c80a83e9644d3de74a6dac542fe8d5f4321a001
Helder De Oliveira <hemarque@gmail.com>
2022-11-28 14:42:45 +0100

    improving for each executing the item updates on the item

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -11,32 +11,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            update(item);
-        }
-    }
-
-    private void update(Item item) {
-        if (isAgedBrie(item)) {
-            item.update();
-        } else if (isBackstagePasses(item)) {
-            item.update();
-        } else if (isSulfuras(item)) {
-            item.update();
-        } else {
             item.update();
         }
     }
 
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
-
-    private boolean isBackstagePasses(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
 }
