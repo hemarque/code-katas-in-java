01bbf15cb34915941c7b66dacf44861488cc229f
Helder De Oliveira <hemarque@gmail.com>
2022-11-28 14:40:57 +0100

    deleting dead code

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -20,7 +20,6 @@ class GildedRose {
             item.update();
         } else if (isBackstagePasses(item)) {
             item.update();
-            //updateBackstagePasses(item);
         } else if (isSulfuras(item)) {
             item.update();
         } else {
@@ -28,59 +27,6 @@ class GildedRose {
         }
     }
 
-    private void updateAgedBrie(Item item) {
-        if (item.quality < 50) {
-            increaseQuality(item);
-        }
-        decreaseSellIn(item);
-        if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                increaseQuality(item);
-            }
-        }
-    }
-
-    private void updateBackstagePasses(Item item) {
-        if (item.quality < 50) {
-            increaseQuality(item);
-            if (item.sellIn < 11) {
-                if (item.quality < 50) {
-                    increaseQuality(item);
-                }
-            }
-            if (item.sellIn < 6) {
-                if (item.quality < 50) {
-                    increaseQuality(item);
-                }
-            }
-        }
-        decreaseSellIn(item);
-        if (item.sellIn < 0) {
-            item.quality = 0;
-        }
-    }
-
-    private void updateSulfuras(Item item) {
-    }
-
-    private void updateDefaultItem(Item item) {
-
-    }
-
-
-    private void decreaseSellIn(Item item) {
-        item.sellIn = item.sellIn - 1;
-    }
-
-    private void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
-    private void increaseQuality(Item item) {
-        item.quality = item.quality + 1;
-    }
 
     private boolean isAgedBrie(Item item) {
         return item.name.equals("Aged Brie");
