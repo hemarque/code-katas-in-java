2a3da8c965b14ee89d8de04a99e060e2485570b1
Helder De Oliveira <hemarque@gmail.com>
2022-11-28 12:04:51 +0100

    sell in ranges

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -17,31 +17,21 @@ class GildedRose {
         if (!isAgedBrie(item)
                 && !isBackstagePasses(item)) {
             if (isQualityGreaterThanZero(item)) {
-                if (!isSulfuras(item)) {
-                    decreaseQuality(item);
-                }
+                decreaseQualityWhenIsNotSulfuras(item);
             }
         } else {
             if (isQualityLessThan50(item)) {
                 increaseQuality(item);
-
                 if (isBackstagePasses(item)) {
-                    if (item.sellIn < 11) {
-                        increaseQuality(item);
-                    }
-
-                    if (isQualityLessThan6(item)) {
-                        increaseQuality(item);
-                    }
+                    increaseQualityWhenIsLessThan11(item);
+                    increaseQualityWhenIsLessThan6(item);
                 }
             }
         }
-
         if (!isSulfuras(item)) {
-            item.sellIn = item.sellIn - 1;
+            decreaseSellIn(item);
         }
-
-        if (item.sellIn < 0) {
+        if (isSellInLessThan0(item)) {
             if (isAgedBrie(item)) {
                 if (isQualityLessThan50(item)) {
                     increaseQuality(item);
@@ -51,13 +41,41 @@ class GildedRose {
                     makeQualityZero(item);
                 } else {
                     if (isQualityGreaterThanZero(item)) {
-                        if (!isSulfuras(item)) {
-                            decreaseQuality(item);
-                        }
+                        decreaseQualityWhenIsNotSulfuras(item);
                     }
                 }
             }
         }
+    }
+
+    private void increaseQualityWhenIsLessThan11(Item item) {
+        if (isSellInLessThan11(item)) {
+            increaseQuality(item);
+        }
+    }
+
+    private void increaseQualityWhenIsLessThan6(Item item) {
+        if (isQualityLessThan6(item)) {
+            increaseQuality(item);
+        }
+    }
+
+    private void decreaseQualityWhenIsNotSulfuras(Item item) {
+        if (!isSulfuras(item)) {
+            decreaseQuality(item);
+        }
+    }
+
+    private boolean isSellInLessThan11(Item item) {
+        return item.sellIn < 11;
+    }
+
+    private boolean isSellInLessThan0(Item item) {
+        return item.sellIn < 0;
+    }
+
+    private void decreaseSellIn(Item item) {
+        item.sellIn = item.sellIn - 1;
     }
 
     private void makeQualityZero(Item item) {
