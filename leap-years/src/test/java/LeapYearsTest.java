d499900e4e986cbdcfa97f688b08fe4b9cd00a46
Helder De Oliveira <hemarque@gmail.com>
2023-10-12 22:06:04 +0100

    not a leap year if is divisible by 100 but not by 400

--- leap-years/src/test/java/LeapYearsTest.java
+++ leap-years/src/test/java/LeapYearsTest.java
@@ -1,6 +1,8 @@
+import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
 public class LeapYearsTest {
@@ -10,8 +12,22 @@ public class LeapYearsTest {
         assertTrue(isLeap(year));
     }
 
+    @Test
+    public void yearIsNotLeapIfDivisibleBy100AndNotBy400() {
+        int year = 1700;
+        assertFalse(isLeap(year));
+    }
+
     private boolean isLeap(int year) {
-        boolean isLeap = year % 400 == 0;
-        return isLeap;
+        return isDivisibleBy400(year) &&
+                !(isDivisibleBy100(year) && !isDivisibleBy400(year));
+    }
+
+    private boolean isDivisibleBy100(int year) {
+        return year % 100 == 0;
+    }
+
+    private boolean isDivisibleBy400(int year) {
+        return year % 400 == 0;
     }
 }
