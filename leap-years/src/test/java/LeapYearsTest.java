28f1b39a2d2b107d66a34867ad2963394789289f
Helder De Oliveira <hemarque@gmail.com>
2023-10-12 21:52:23 +0100

    including parameterized tests

--- leap-years/src/test/java/LeapYearsTest.java
+++ leap-years/src/test/java/LeapYearsTest.java
@@ -1,11 +1,12 @@
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static junit.framework.TestCase.assertTrue;
 
 public class LeapYearsTest {
-    @Test
-    public void beLeapIfIsDivisibleBy400(){
-        int year = 400;
+    @ParameterizedTest
+    @ValueSource(ints = {400, 800, 1200, 1600, 2000})
+    public void yearIsLeapIfIsDivisibleBy400(int year) {
 
         boolean isLeap = year % 400 == 0;
 
